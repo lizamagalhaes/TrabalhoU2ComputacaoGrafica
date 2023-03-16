d8dac0f7eced9025d3ac155871fdcf834a1705bb
Liza Magalhaes <liza.magalhaes@hotmail.com>
2023-03-15 22:48:05 -0300

    preeenche circulo parametrico

--- src/main/kotlin/computacao/grafica/DesenhaCirculoParametrica.kt
+++ src/main/kotlin/computacao/grafica/DesenhaCirculoParametrica.kt
@@ -6,22 +6,33 @@ import javax.swing.JPanel
 import kotlin.math.cos
 import kotlin.math.sin
 
-class DesenhaCirculoParametrica(val eixoX: Int, val eixoY: Int, val raio: Int) : JPanel() {
+class DesenhaCirculoParametrica(val eixoX: Int, val eixoY: Int, var raio: Int) : JPanel() {
     override fun paint(g: Graphics?) {
         super.paint(g)
+        g?.let { graficos ->
+            graficos.color = Color.BLUE
+            desenhaLinha(graficos)
+            preencherCirculo(graficos)
+        }
+    }
+
+    private fun desenhaLinha(graficos: Graphics) {
         val cx = eixoX / 2
         val cy = eixoY / 2
         var theta = 0.0
+        while (theta <= 2 * Math.PI) {
+            val x = (cx + raio * cos(theta)).toInt()
+            val y = (cy + raio * sin(theta)).toInt()
+            graficos.drawLine(x, y, x, y)
+            theta += 0.01
+        }
+    }
 
-        g?.let { graficos ->
-            graficos.color = Color.BLUE
-            while (theta <= 2 * Math.PI) {
-                val x = (cx + raio * cos(theta)).toInt()
-                val y = (cy + raio * sin(theta)).toInt()
-                graficos.drawLine(x, y, x, y)
-                theta += 0.01
-            }
-            //graficos.fillOval(cx - raio, cy - raio, 2 * raio, 2 * raio)
+    private fun preencherCirculo(graficos: Graphics) {
+        graficos.color = Color.PINK
+        while (raio > 0) {
+            raio--
+            desenhaLinha(graficos)
         }
     }
 }
