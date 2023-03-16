5aaac95e3196d9a3a7a21ae5d33834284c5927ed
Liza Magalhaes <liza.magalhaes@hotmail.com>
2023-03-15 23:14:59 -0300

    add draw elipse

--- src/main/kotlin/computacao/grafica/DesenhaElipseBresenhan.kt
+++ src/main/kotlin/computacao/grafica/DesenhaElipseBresenhan.kt
@@ -1,55 +1,72 @@
 package computacao.grafica
 
+import java.awt.Color
 import java.awt.Graphics
 import javax.swing.JPanel
 
-class DesenhaElipseBresenhan(val eixoX: Int, val eixoY: Int,val raioEmX: Int, val raioEmY: Int) : JPanel() {
+class DesenhaElipseBresenhan(val eixoX: Int, val eixoY: Int, var raioEmX: Int, var raioEmY: Int) : JPanel() {
 
     private var ponto: Int = 0
     private var auxPonto: Int = 0
     override fun paint(g: Graphics?) {
         super.paint(g)
+        g?.let { graficos ->
+            graficos.color = Color.BLACK
+            desenhaElipse(graficos)
+            preencheElipse(graficos)
+        }
+    }
+
+    private fun desenhaElipse(graficos: Graphics) {
         val cx = eixoX / 2
         val cy = eixoY / 2
         var x = 0
         var y = raioEmY
 
-        ponto = raioEmX * raioEmY - raioEmX * raioEmX * raioEmY + raioEmX * raioEmX/4
+        ponto = raioEmX * raioEmY - raioEmX * raioEmX * raioEmY + raioEmX * raioEmX / 4
 
-        while (2* raioEmY * raioEmY * x <= 2 * raioEmX * raioEmX * y) {
-            g?.drawLine(cx + x, cy + y, cx + x, cy + y)
-            g?.drawLine(cx - x, cy + y, cx - x, cy + y)
-            g?.drawLine(cx + x, cy - y, cx + x, cy - y)
-            g?.drawLine(cx - x, cy - y, cx - x, cy - y)
+        while (2 * raioEmY * raioEmY * x <= 2 * raioEmX * raioEmX * y) {
+            graficos.drawLine(cx + x, cy + y, cx + x, cy + y)
+            graficos.drawLine(cx - x, cy + y, cx - x, cy + y)
+            graficos.drawLine(cx + x, cy - y, cx + x, cy - y)
+            graficos.drawLine(cx - x, cy - y, cx - x, cy - y)
 
             if (ponto < 0) {
                 x++
-                ponto = ponto + 2 * raioEmY * raioEmY * x + raioEmY * raioEmY
-            }
-            else {
+                ponto += 2 * raioEmY * raioEmY * x + raioEmY * raioEmY
+            } else {
                 x++
                 y--
                 ponto = ponto + 2 * raioEmY * raioEmY * x - 2 * raioEmX * raioEmX * y - raioEmY * raioEmY
             }
         }
 
-        auxPonto = (raioEmY * raioEmY * (x + 0.5) * (x + 0.5) + raioEmX * raioEmX * (y - 1) * (y - 1) - raioEmX * raioEmX * raioEmY * raioEmY).toInt()
+        auxPonto = (raioEmY * raioEmY * (x + 0.5) * (x + 0.5)
+                + raioEmX * raioEmX * (y - 1) * (y - 1) - raioEmX * raioEmX * raioEmY * raioEmY).toInt()
 
         while (y >= 0) {
-            g?.drawLine(cx + x, cy + y, cx + x, cy + y)
-            g?.drawLine(cx - x, cy + y, cx - x, cy + y)
-            g?.drawLine(cx + x, cy - y, cx + x, cy - y)
-            g?.drawLine(cx - x, cy - y, cx - x, cy - y)
+            graficos.drawLine(cx + x, cy + y, cx + x, cy + y)
+            graficos.drawLine(cx - x, cy + y, cx - x, cy + y)
+            graficos.drawLine(cx + x, cy - y, cx + x, cy - y)
+            graficos.drawLine(cx - x, cy - y, cx - x, cy - y)
 
-            if (auxPonto > 0) {
+            auxPonto = if (auxPonto > 0) {
                 y--
-                auxPonto = auxPonto - 2 * raioEmX * raioEmX * y + raioEmX * raioEmX
-            }
-            else {
+                auxPonto - 2 * raioEmX * raioEmX * y + raioEmX * raioEmX
+            } else {
                 x++
                 y--
-                auxPonto = auxPonto + 2 * raioEmY * raioEmY * x - 2 * raioEmX * raioEmX * y - raioEmX * raioEmX
+                auxPonto + 2 * raioEmY * raioEmY * x - 2 * raioEmX * raioEmX * y - raioEmX * raioEmX
             }
+        }
+    }
+
+    private fun preencheElipse(graficos: Graphics) {
+        graficos.color = Color.PINK
+        while ((raioEmX > 0).and(raioEmY > 0)) {
+            raioEmX--
+            raioEmY--
+            desenhaElipse(graficos)
         }
     }
 }
